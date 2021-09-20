02add9d1344e4ca44f8026dfef8c366de1143192
Sarek Høverstad Skotåm <sarekhs@uio.no>
2021-09-20 02:50:05 +0200

    did 17

--- 17/day17.rs
+++ 17/day17.rs
@@ -1,103 +1,258 @@
-type Layout = Vec<Vec<char>>;
-
-#[aoc_generator(day17)]
-pub fn input_generator(input: &str) -> Vec<Layout> {
-    let mut out: Vec<Layout> = Vec::new();
-    for i in 0..6{
-
-    input
-        .lines()
-        .map(|l| l.chars().collect::<Vec<char>>())
-        .collect()
-}
-
-fn value(input: Vec<&Layout>, i: i32, j: i32, k: i32) -> i32 {
-    match input.get(i as usize){
-        None => 0,
-        x => match x.unwrap.get(j as usize){
-            None => 0,
-            y => match y.unwrap().get(k as usize){
-                None => 0,
-                Some('#') => 1,
-                _ => 0,
-            }
-        }
-}
-
-fn count(input: Vec<&Layout>, x: i32, y: i32, z: i32) -> i32 {
-    let mut sum = -value(&input, x, y, z);
-    for i in 0..3{
-        for j in 0..3{
-            for k in 0..3{
-                sum += value(&input, x-1+i, y-1+j, z-1+k);
-            }
-        }
-    }
-    sum-=value(&input, x, y, z);
-    sum
-}
-
 #[aoc(day17, part1)]
-pub fn solve_part1(input: Vec<&Layout>) -> usize {
-    let mut old: Vec<Layout> = Vec::new();
-    old.push(input.clone());
-    for i in 0..6 {
-        let mut new: Vec<Layout> = Vec::new();
-        for i in 0..old.len() {
-            let mut vec = Vec::new();
-            for j in 0..old[i].len() {
-               vec.push(match old[i][j] {
-                    '.' => '.',
-                    '#' => if count(&old, i, j) >= 4 { updated = true;'L' } else { '#' },
-                    'L' => if count(&old, i, j) == 0 { updated = true;'#' } else { 'L' },
-                    _ => panic!("Invalid char"),
-                });
+pub fn solve_part1(input: &str) -> i32 {
+    let PADDING = 64;
+    let mut cubes = vec![];
+    for i in 0..32 {
+        let mut plane = vec![];
+        for e in 0..PADDING {
+            let mut curr = vec![];
+            for e2 in 0..PADDING {
+                curr.push(0);
             }
-            new.push(vec);
+            plane.push(curr);
         }
-        old = new.clone()
+        cubes.push(plane);
     }
-    old.iter().flatten().filter(|&n| *n == '#').count()
-}
-
-fn value_2(input: &Layout, i: usize, j: usize, i_dir: i32, j_dir: i32) -> u32 {
-    match input.get(i){
-        None => 0,
-        y => match y.unwrap().get(j){
-            None | Some('L') => 0,
-            Some('#') => 1,
-            _ => value_2(&input, (i as i32 + i_dir) as usize, (j as i32 + j_dir) as usize, i_dir, j_dir),
+    let mut inp = vec![];
+    for _ in 0..32 {
+        let mut curr = vec![];
+        for _ in 0..PADDING {
+            curr.push(0);
+        }
+        inp.push(curr);
+    }
+    for l in input.lines() {
+        let mut curr = vec![];
+        for e in 0..28 {
+            curr.push(0);
+        }
+        for e in l.chars() {
+            if e == '#' {
+                curr.push(1);
+            }
+            else if e == '.' {
+                curr.push(0);
+            }
+        }
+        for e in 0..28 {
+            curr.push(0);
+        }
+        inp.push(curr);
+    }
+    for _ in 0..32 {
+        let mut curr = vec![];
+        for _ in 0..PADDING {
+            curr.push(0);
+        }
+        inp.push(curr);
+    }
+    cubes.push(inp);
+    for i in 0..32 {
+        let mut plane = vec![];
+        for e in 0..PADDING {
+            let mut curr = vec![];
+            for e2 in 0..PADDING {
+                curr.push(0);
+            }
+            plane.push(curr);
+        }
+        cubes.push(plane);
+    }
+    for m in 0..6 {
+        let mut ilayer = vec![];
+        for i in 1..PADDING-1 {
+            let mut jlayer = vec![];
+            for j in 1..PADDING-1 {
+                let mut klayer = vec![];
+                for k in 1..PADDING-1 {
+                    let mut neigh_cnt = -cubes[i][j][k];
+                    for i2 in -1..2 {
+                        for j2 in -1..2 {
+                            for k2 in -1..2 {
+                                neigh_cnt = neigh_cnt + cubes[(i as i32 +i2) as usize][(j as i32+j2) as usize][(k as i32+k2) as usize];
+                            }
+                        }
+                    }
+                    if neigh_cnt == 3 {
+                        klayer.push(1);
+                    }
+                    else if neigh_cnt == 2 {
+                        klayer.push(cubes[i][j][k]);
+                    }
+                    else {
+                        klayer.push(0);
+                    }
+                }
+                jlayer.push(klayer);
+            }
+            ilayer.push(jlayer);
+        }
+        for i in 0..PADDING-2 {
+            for j in 0..PADDING-2 {
+                for k in 0..PADDING-2 {
+                    cubes[i][j][k] = ilayer[i][j][k];
+                }
+            }
         }
     }
-}
-
-fn count_2(input: &Layout, i: usize, j: usize) -> u32 {
-    value_2(&input, i-1, j-1, -1, -1) + value_2(&input, i-1, j, -1, 0) + value_2(&input, i-1, j+1, -1, 1) +
-    value_2(&input, i, j-1, 0, -1) + value_2(&input, i, j+1, 0, 1) +
-    value_2(&input, i+1, j-1, 1, -1) + value_2(&input, i+1, j, 1, 0) + value_2(&input, i+1, j+1, 1, 1)
+    let mut rtot = 0;
+    for i in 0..PADDING {
+        for j in 0..PADDING {
+            for k in 0..PADDING {
+                rtot += cubes[i][j][k];
+            }
+        }
+    }
+    rtot
 }
 
 #[aoc(day17, part2)]
-pub fn solve_part2(input: &Layout) -> usize {
-    let mut updated = true;
-    let mut old: Vec<Vec<char>> = input.clone();
-    while updated {
-        updated = false;
-        let mut new: Vec<Vec<char>> = Vec::new();
-        for i in 0..old.len() {
-            let mut vec = Vec::new();
-            for j in 0..old[i].len() {
-               vec.push(match old[i][j] {
-                    '.' => '.',
-                    '#' => if count_2(&old, i, j) >= 5 { updated = true;'L' } else { '#' },
-                    'L' => if count_2(&old, i, j) == 0 { updated = true;'#' } else { 'L' },
-                    _ => panic!("Invalid char"),
-                });
+pub fn solve_part2(input: &str) -> i32 {
+    let PADDING = 64;
+    let mut cubes = vec![];
+    for i in 0..32 {
+        let mut planes = vec![];
+        for _ in 0..PADDING {
+            let mut plane = vec![];
+            for e in 0..PADDING {
+                let mut curr = vec![];
+                for e2 in 0..PADDING {
+                    curr.push(0);
+                }
+                plane.push(curr);
             }
-            new.push(vec);
+            planes.push(plane);
         }
-        old = new.clone()
+        cubes.push(planes);
     }
-    old.iter().flatten().filter(|&n| *n == '#').count()
+    let mut inp = vec![];
+    for _ in 0..32 {
+        let mut plane = vec![];
+        for _ in 0..PADDING {
+            let mut curr = vec![];
+            for _ in 0..PADDING {
+                curr.push(0);
+            }
+            plane.push(curr);
+        }
+        inp.push(plane);
+    }
+    let mut plane = vec![];
+    for _ in 0..32 {
+        let mut curr = vec![];
+        for _ in 0..PADDING {
+            curr.push(0);
+        }
+        plane.push(curr);
+    }
+    for l in input.lines() {
+        let mut curr = vec![];
+        for e in 0..28 {
+            curr.push(0);
+        }
+        for e in l.chars() {
+            if e == '#' {
+                curr.push(1);
+            }
+            else if e == '.' {
+                curr.push(0);
+            }
+        }
+        for e in 0..28 {
+            curr.push(0);
+        }
+        plane.push(curr);
+    }
+    for _ in 0..PADDING {
+        let mut curr = vec![];
+        for _ in 0..PADDING {
+            curr.push(0);
+        }
+        plane.push(curr);
+    }
+    inp.push(plane);
+    for _ in 0..32 {
+        let mut plane = vec![];
+        for _ in 0..PADDING {
+            let mut curr = vec![];
+            for _ in 0..PADDING {
+                curr.push(0);
+            }
+            plane.push(curr);
+        }
+        inp.push(plane);
+    }
+    cubes.push(inp);
+    for i in 0..32 {
+        let mut planes = vec![];
+        for _ in 0..PADDING {
+            let mut plane = vec![];
+            for e in 0..PADDING {
+                let mut curr = vec![];
+                for e2 in 0..PADDING {
+                    curr.push(0);
+                }
+                plane.push(curr);
+            }
+            planes.push(plane);
+        }
+        cubes.push(planes);
+    }
+    for m in 0..6 {
+        let mut ilayer = vec![];
+        for i in 1..PADDING-1 {
+            let mut jlayer = vec![];
+            for j in 1..PADDING-1 {
+                let mut klayer = vec![];
+                for k in 1..PADDING-1 {
+                    let mut hlayer = vec![];
+                    for h in 1..PADDING-1 {
+                        let mut neigh_cnt = -cubes[i][j][k][h];
+                        for i2 in -1..2 {
+                            for j2 in -1..2 {
+                                for k2 in -1..2 {
+                                    for h2 in -1..2 {
+                                        neigh_cnt = neigh_cnt + cubes[(i as i32 +i2) as usize][(j as i32+j2) as usize][(k as i32+k2) as usize][(h as i32+h2) as usize];
+                                    }
+                                }
+                            }
+                        }
+                        if neigh_cnt == 3 {
+                            hlayer.push(1);
+                        }
+                        else if neigh_cnt == 2 {
+                            hlayer.push(cubes[i][j][k][h]);
+                        }
+                        else {
+                            hlayer.push(0);
+                        }
+                    }
+                    klayer.push(hlayer);
+                }
+                jlayer.push(klayer);
+            }
+            ilayer.push(jlayer);
+        }
+        for i in 0..PADDING-2 {
+            for j in 0..PADDING-2 {
+                for k in 0..PADDING-2 {
+                    for h in 0..PADDING-2 {
+                        cubes[i][j][k][h] = ilayer[i][j][k][h];
+                    }
+                }
+            }
+        }
+    }
+    let mut rtot = 0;
+    for i in 0..PADDING {
+        for j in 0..PADDING {
+            for k in 0..PADDING {
+                for h in 0..PADDING {
+                    rtot += cubes[i][j][k][h];
+                }
+            }
+        }
+    }
+    rtot
 }
 
