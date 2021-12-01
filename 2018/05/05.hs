caf07a6ae507ba3294229815a5666efbff5a43a1
Sarek Høverstad Skotåm <sarek.skotam@gmail.com>
2021-12-02 02:10:06 +0700

    Did day 5 2018

---
+++ 2018/05/05.hs
@@ -0,0 +1,24 @@
+import Data.Char
+
+converge :: (a -> a -> Bool) -> [a] -> a
+converge p (x:ys@(y:_))
+    | p x y     = y
+    | otherwise = converge p ys
+
+solver [] = []
+solver ([x]) = [x]
+solver (x:y:xs)
+    | isLower y && x == toUpper y = solver xs
+    | isLower x && y == toUpper x = solver xs
+    | otherwise = x : (solver $ y : xs)
+
+full x = length $ converge (==) $ iterate solver x
+
+pre inp o = filter (\x -> x /= o && x /= toUpper o) inp
+
+main = do
+    input <- readFile "input.txt"
+    let l = init input
+    print $ full l
+    let alph = "abcdefghijklmnopqrstuvwxyz"
+    print $ map full (map (pre l) alph)
---
+++ 2018/05/README.md
@@ -0,0 +1,13 @@
+## Day 5
+
+### Part 1
+
+I spent way too long realizing that I needed the `isLower` check in `solver`.
+Other than that, quite happy.
+
+### Part 2
+
+Went way quicker than part 1. I am quite happy with this one. Could probably be
+made nicer if I knew my combinators, but other than that, pretty happy overall.
+
+Solution takes forever though, but I'm not sure if that is me or Haskell.
