2932c63bfa816290c8fe123c30c3b9eaa5d589ec
Sarek Høverstad Skotåm <sarek.skotam@gmail.com>
2021-12-01 15:08:11 +0700

    Update day1 2021

--- 2021/01/01.hs
+++ 2021/01/01.hs
@@ -3,11 +3,13 @@ parse = map read . lines
 
 solveA :: [Int] -> Int
 solveA input = do
-    (-1 +) $ length $ filter (\(x,y) -> x > y) $ zip input (-1 : input)
+    -- (-1 +) $ length $ filter (\(x,y) -> x > y) $ zip input (-1 : input)
+    length $ filter (\(x,y) -> x < y) $ zip input (tail input)
 
 solveB :: [Int] -> Int
 solveB input = do
-    solveA $ drop 2 $ zipWith3 (\x y z -> x + y + z) input (0 : input) (0 : 0 : input)
+    -- solveA $ drop 2 $ zipWith3 (\x y z -> x + y + z) input (0 : input) (0 : 0 : input)
+    solveA $ zipWith3 (\x y z -> x + y + z) input (tail input) (drop 2 input)
 
 main = do
     input <- parse <$> readFile "input.txt"
--- 2021/01/README.md
+++ 2021/01/README.md
@@ -18,3 +18,11 @@ It isn't too bad, but it isn't great either. I don't like the overcounting and
 There does probably exist nicer solutions, but I am overall not too dissatisfied
 with this one. Would have liked it to be able to work with any size window, I
 guess I have to learn how to do that. I like the reuse of `solveA`.
+
+#### Edit
+
+After looking at [Sondres
+solution](https://github.com/sondresl/AdventOfCode/blob/master/2021/Haskell/src/Day01.hs),
+I realized that I could use `tail` to remove the need to do `(-1 +)` in the end,
+and similarily not have to `drop` for part 2. A small improvement, but an
+improvement nonetheless.
