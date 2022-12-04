a2fae4e8260f82d4ecaeba587b79d25e5e956475
Sarek Høverstad Skotåm <sarek.skotam@gmail.com>
2022-12-04 06:54:05 +0000

    Day 4 2022

---
+++ 2022/04/04.py
@@ -0,0 +1,12 @@
+part1 = 0
+part2 = 0
+for line in open("input.txt").readlines():
+    l, r = line.strip().split(",")
+    ll, lr = map(int, l.split("-"))
+    rl, rr = map(int, r.split("-"))
+    if (ll <= rl and lr >= rr) or (rl <= ll and rr >= lr):
+        part1 += 1
+    if max(ll, rl) <= min(lr, rr):
+        part2 += 1
+print(part1)
+print(part2)
