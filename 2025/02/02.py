e4b4a300904204c02ea37d1ea7a2c4e932f85f7c
Sarek Høverstad Skotåm <sarek.skotam@gmail.com>
2025-12-03 01:33:34 -0800

    2025 Day 3

--- 2025/02/02.py
+++ 2025/02/02.py
@@ -6,7 +6,7 @@ for (l, r) in [map(int, l.split("-")) for l in open("input.txt").read().split(",
         s = str(e)
         if s[:len(s)//2] == s[len(s)//2:]:
             p1 += e
-        for n in range(1, (len(s)//2)+1):
+        for n in range(1, len(s)//2+1):
             if len(set(textwrap.wrap(s, n))) == 1:
                 p2 += e
                 break
---
+++ 2025/03/03.py
@@ -0,0 +1,19 @@
+p1 = 0
+p2 = 0
+
+for l in open("input.txt").read().split():
+    f = max(l[:-1])
+    s = max(l[l.index(f)+1:])
+    p1 += int(str(f) + str(s))
+
+    tmp = ""
+    index = 0
+    for i in range(0, 12):
+        sub = l[index:len(l) - 11 + i]
+        n = max(sub)
+        index += sub.index(n) + 1 
+        tmp += str(n)
+    p2 += int(tmp)
+
+print(p1)
+print(p2)
